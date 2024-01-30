3141c20648c14da96a86d5c0b464536c46245cce
Mehmetdora <mehmetdora333@gmail.com>
2024-01-30 16:55:08 +0300

    Initial Commit

---
+++ new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Mehmet Dora on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
